c44eb634ef8c69cac21df8eaa8949ea64e92c184
P4chenk4 <109737082+P4chenk4@users.noreply.github.com>
2022-07-31 14:26:47 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,31 @@
+__all__ = [
+	'API_TOKEN',
+	'WEBHOOK_HOST',
+	'WEBHOOK_PATH',
+	'WEBAPP_HOST',
+	'WEBAPP_PORT',
+	'DBNAME',
+	'USER',
+	'PASSWORD',
+	'HOST',
+	'TABLENAME'
+]
+
+
+#Telegram bot token
+API_TOKEN = '5596410114:AAH3pAvdIR9u9TBVsgMjwDKB2vat_q3mYN8'
+
+#WEbhook
+WEBHOOK_HOST = 'https://5936-178-54-130-218.eu.ngrok.io'
+WEBHOOK_PATH = '/'
+
+#WebAPP
+WEBAPP_HOST = 'localhost'
+WEBAPP_PORT = 3001
+
+#PostgreSQL settings
+DBNAME = 'database'
+USER = 'admin'
+PASSWORD = 'admin'
+HOST = 'localhost'
+TABLENAME = 'telegramdb'
--- main.py
+++ main.py
@@ -1,37 +1,59 @@
 import psycopg2
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.utils.executor import start_webhook
+from config import *
 
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
-bot = Bot(token="")
+WEBAPP_HOST = 'localhost'
+WEBAPP_PORT = 3001
+
+bot = Bot(token=API_TOKEN,)
 dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
 
 try:
 	conn = psycopg2.connect(
-		dbname='',
-		user='',
-		password='',
-		host=''
+		dbname=DBNAME,
+		user=USER,
+		password=PASSWORD,
+		host=HOST
 	)
 	conn.autocommit = True
 	cursor = conn.cursor()
+	print("[INFO] Connection to PostgreSQL success!")
 except Exception as _ex:
-	print("[INFO] Connection to PostgreSQL failed!")
-
-
+	print("[ERROR] Connection to PostgreSQL failed!")
 
 @dp.message_handler()
 async def message(message: types.Message):
-	await	message.answer("Привет!")
+	await bot.send_message(message.chat.id, "Привет!")
 	firstname = message.from_user.first_name
 	username = message.from_user.username
 	userid = str(message.from_user.id)
-	cursor.execute('SELECT first_name FROM users WHERE userid = %s', (userid,))
+	cursor.execute('SELECT first_name FROM '+TABLENAME+' WHERE userid = %s', (userid,))
 	if cursor.fetchone() == None:
-		cursor.execute('INSERT INTO users (first_name, username, userid) VALUES (%s, %s, %s)', (firstname, username, userid))
-		print("[INFO] New user "+firstname+".")
+		cursor.execute('INSERT INTO '+TABLENAME+' (first_name, username, userid) VALUES (%s, %s, %s)', (firstname, username, userid))
+		print("[INFO] New user "+firstname+" added to database.")
 	else:
-		print("[INFO] User "+firstname+".")
+		print("[INFO] User "+firstname+" send message.")
 
 
-if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+
+async def on_shutdown(dp):
+    logging.warning('Shutting down..')
+    await bot.delete_webhook()
+
+if __name__ == '__main__':
+	start_webhook(
+    	dispatcher=dp, 
+  		webhook_path=WEBHOOK_PATH,
+  		on_startup=on_startup,
+  		on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT
+    )
